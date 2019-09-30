777fa5ffead540e64d0a7cbee2543e70541ab297
Ryanet11 <49400375+Ryanet11@users.noreply.github.com>
2019-09-30 22:10:48 +0200

    complete redo

---
+++ assets/js/logo.js
@@ -0,0 +1,9 @@
+window.addEventListener("resize", function() {
+    if (window.innerWidth > 980) {
+        document.getElementById("logo").innerHTLM = "<img src='images/logo.png' width='80' height='80' alt='RoyalMining logo'/>";
+    } else {
+        document.getElementById("logo").innerHTML= "RoyalMining";
+    }
+});
+//document.getElementById("logo").innerHTLM = "<img src='images/logo.png' width='80' height='80' alt='RoyalMining logo'/>";
+//document.getElementById("logo").innerHTML= "RoyalMining"
